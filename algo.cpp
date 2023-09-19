6b2cb178ccacdd81ff96f764ff8213b224c7674c
haelime <gnostalgiclife@gmail.com>
2023-09-19 17:04:14 +0900

    init
    init

---
+++ algo.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <cstdio>
+#define ll long long
+#define ld long double
+#define FASTIO                        \
+	ios_base::sync_with_stdio(false); \
+	cin.tie(0)
+#define MOD 1'000'000'007
+#define sz(v) ((int)(v).size())
+#define all(v) (v).begin(), (v).end()
+#define INF (int)(~0U >> 2)
+using namespace std;
+using pi = pair<int, int>;
+int main(void)
+{
+	FASTIO;
+	
+	return 0;
+}
